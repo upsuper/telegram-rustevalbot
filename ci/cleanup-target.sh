8df8fff4cea5e7628804a96734b2eb37cfd619ae
Xidorn Quan <me@upsuper.org>
2018-08-21 16:16:06 +1000

    Put target cleanup into before_cache phase

--- ci/cleanup-target.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd $TRAVIS_BUILD_DIR/target
-rm -rf {debug,release}/{,lib}telegram-rustevalbot*
-rm -rf {debug,release}/telegram_rustevalbot-*
-rm -rf {debug,release}/{build,.fingerprint}/telegram-rustevalbot-*
-rm -rf {debug,release}/deps/telegram_rustevalbot-*
-rm -rf */{debug,release}/.fingerprint/telegram-rustevalbot-*
-rm -rf debug/incremental
