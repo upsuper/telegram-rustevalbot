e0bf0d3aab6d47cc54b64786535c09902d90f2a6
Xidorn Quan <me@upsuper.org>
2018-09-15 11:32:02 +1000

    Add debug assertion for no special character in path.
    This fixes #30.

--- src/command/doc.rs
+++ src/command/doc.rs
@@ -15,6 +15,18 @@ lazy_static! {
     static ref SEEKER: RustDocSeeker = {
         let data = fs::read_to_string("search-index.js").expect("cannot find search-index.js");
         let doc: RustDoc = data.parse().expect("cannot parse search-index.js");
+        if cfg!(debug_assertions) {
+            const SPECIAL_CHARS: &[char] = &['<', '>', '"', '\'', '&'];
+            for item in doc.iter() {
+                // If there is any HTML special character in item path,
+                // we need to properly escape them in DocItemExt::write_item.
+                if item.path.contains(SPECIAL_CHARS) ||
+                    item.parent.as_ref().map_or(false, |p| p.as_ref().contains(SPECIAL_CHARS)) ||
+                    item.name.as_ref().contains(SPECIAL_CHARS) {
+                    panic!("Found path with HTML special character: {:?}", item);
+                }
+            }
+        }
         doc.build()
     };
 }
@@ -166,7 +178,8 @@ impl DocItemExt for DocItem {
         output.write_str(r#"">"#)?;
         // Write full path. We don't escape them and we pass them into
         // WidthCountingWriter directly assuming they don't contain any
-        // HTML special characters.
+        // HTML special characters. This is checked in debug assertions
+        // in the lazy_static block above.
         let ty = ItemType::from(&self.name);
         let path_width = {
             let mut output = WidthCountingWriter::new(&mut output);
