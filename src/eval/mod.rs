0de96fc4a6eb4e973c9d267deb5befebdb56898d
Xidorn Quan <me@upsuper.org>
2018-11-30 23:07:28 +1100

    Merge eval::processor mod into eval mod
    It makes no sense to have a separate processor mod when eval bot itself
    is a separate mod.

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -1,49 +1,167 @@
-use self::processor::Processor;
+use self::command::{Command, Executor};
+use self::record::RecordService;
 use crate::bot::{Bot, Error};
 use crate::shutdown::Shutdown;
+use crate::ADMIN_ID;
 use futures::{Future, IntoFuture};
-use log::{debug, info};
+use log::{debug, info, warn};
+use matches::matches;
 use reqwest::r#async::Client;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::rc::Rc;
 use std::sync::Arc;
-use telegram_types::bot::types::{Update, UpdateId};
+use telegram_types::bot::types::{ChatType, Message, Update, UpdateContent, UpdateId};
 
 mod command;
-mod processor;
 mod record;
 
 pub use self::command::init;
 
+/// Eval bot.
 pub struct EvalBot {
-    processor: Processor,
+    bot: Bot,
+    executor: Executor,
+    records: Rc<RefCell<RecordService>>,
     shutdown_id: Rc<Cell<Option<UpdateId>>>,
 }
 
+type BoxFuture = Box<dyn Future<Item = (), Error = ()>>;
+
 impl EvalBot {
+    /// Create new eval bot instance.
     pub fn new(client: Client, bot: Bot, shutdown: Arc<Shutdown>) -> Self {
         let shutdown_id = Rc::new(Cell::new(None));
-        let executor = command::Executor::new(client, bot.username, shutdown, shutdown_id.clone());
-        let processor = Processor::new(bot, executor);
-        info!("EvalBot authorized as @{}", processor.bot().username);
+        let executor = Executor::new(client, bot.username, shutdown, shutdown_id.clone());
+        let records = Rc::new(RefCell::new(RecordService::init()));
+        info!("EvalBot authorized as @{}", bot.username);
         EvalBot {
-            processor,
+            bot,
+            executor,
+            records,
             shutdown_id,
         }
     }
 
-    pub fn handle_update(&self, update: Update) -> Box<dyn Future<Item = (), Error = ()>> {
-        self.processor.handle_update(update)
+    /// Borrow the underlying bot.
+    pub fn bot(&self) -> &Bot {
+        &self.bot
+    }
+
+    /// Handle the update.
+    pub fn handle_update(&self, update: Update) -> BoxFuture {
+        let id = update.update_id;
+        match update.content {
+            UpdateContent::Message(message) => self.handle_message(id, &message),
+            UpdateContent::EditedMessage(message) => self.handle_edit_message(id, &message),
+            _ => Box::new(Ok(()).into_future()),
+        }
     }
 
     pub fn shutdown(self) -> Box<dyn Future<Item = (), Error = Error>> {
         if let Some(shutdown_id) = self.shutdown_id.take() {
             debug!("{}> confirming", shutdown_id.0);
-            let bot = self.processor.bot();
+            let bot = self.bot();
             return Box::new(bot.confirm_update(shutdown_id).map(move |_| {
                 debug!("{}> confirmed", shutdown_id.0);
             }));
         }
         Box::new(Ok(()).into_future())
+    }
+
+    fn handle_message(&self, id: UpdateId, message: &Message) -> BoxFuture {
+        self.records.borrow_mut().clear_old_records(&message.date);
+        let cmd = match Self::build_command(id, message) {
+            Ok(cmd) => cmd,
+            Err(()) => return Box::new(Ok(()).into_future()),
+        };
+        let msg_id = message.message_id;
+        self.records
+            .borrow_mut()
+            .push_record(msg_id, message.date.clone());
+        let chat_id = message.chat.id;
+        let bot = self.bot.clone();
+        let records = self.records.clone();
+        match self.executor.execute(&cmd) {
+            Some(future) => Box::new(future.then(move |reply| {
+                let reply = reply.unwrap();
+                let reply = reply.trim_matches(char::is_whitespace);
+                debug!("{}> sending: {:?}", id.0, reply);
+                bot.send_message(chat_id, reply)
+                    .execute()
+                    .map(move |msg| {
+                        let reply_id = msg.message_id;
+                        debug!("{}> sent as {}", id.0, reply_id.0);
+                        records.borrow_mut().set_reply(msg_id, reply_id);
+                    }).map_err(move |err| warn!("{}> error: {:?}", id.0, err))
+            })),
+            None => Box::new(Err(()).into_future()),
+        }
+    }
+
+    fn handle_edit_message(&self, id: UpdateId, message: &Message) -> BoxFuture {
+        let cmd = match Self::build_command(id, message) {
+            Ok(cmd) => cmd,
+            // XXX Can this happen at all? Can a text message becomes other types?
+            Err(()) => return Box::new(Ok(()).into_future()),
+        };
+        let msg_id = message.message_id;
+        let reply_id = match self.records.borrow().find_reply(msg_id) {
+            Some(reply) => reply,
+            None => return Box::new(Ok(()).into_future()),
+        };
+
+        let chat_id = message.chat.id;
+        let bot = self.bot.clone();
+        let records = self.records.clone();
+        match self.executor.execute(&cmd) {
+            Some(future) => Box::new(future.then(move |reply| {
+                let reply = reply.unwrap();
+                let reply = reply.trim_matches(char::is_whitespace);
+                debug!("{}> updating: {:?}", id.0, reply);
+                bot.edit_message(chat_id, reply_id, reply)
+                    .execute()
+                    .map(move |_| debug!("{}> updated", id.0))
+                    .map_err(move |err| warn!("{}> error: {:?}", id.0, err))
+            })),
+            None => Box::new({
+                debug!("{}> deleting", id.0);
+                records.borrow_mut().remove_reply(msg_id);
+                bot.delete_message(chat_id, reply_id)
+                    .execute()
+                    .map(move |_| debug!("{}> deleted", id.0))
+                    .map_err(move |err| warn!("{}> error: {:?}", id.0, err))
+            }),
+        }
+    }
+
+    fn build_command(id: UpdateId, message: &Message) -> Result<Command, ()> {
+        // Don't care about messages not sent from a user.
+        let from = match message.from.as_ref() {
+            Some(from) => from,
+            _ => return Err(()),
+        };
+        // Don't care about non-text messages.
+        let command = match &message.text {
+            Some(text) => text,
+            _ => return Err(()),
+        };
+        debug!(
+            "{}> received from {}({}): [{}] {:?}",
+            id.0,
+            from.username
+                .as_ref()
+                .map_or("[no username]", |s| s.as_str()),
+            from.id.0,
+            message.message_id.0,
+            command
+        );
+        let is_admin = ADMIN_ID.map_or(false, |admin_id| admin_id == from.id);
+        let is_private = matches!(message.chat.kind, ChatType::Private { .. });
+        Ok(Command {
+            id,
+            command,
+            is_admin,
+            is_private,
+        })
     }
 }
