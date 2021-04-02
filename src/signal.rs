1eccdb8ecffff6859fc2bf4fccbe121db353925e
Xidorn Quan <me@upsuper.org>
2021-04-02 15:23:04 +1100

    Upgrade signal-hook

--- src/signal.rs
+++ src/signal.rs
@@ -1,12 +1,12 @@
 use crate::shutdown::Shutdown;
 use log::info;
+use signal_hook::consts::{SIGINT, SIGTERM};
 use signal_hook::iterator::Signals;
-use signal_hook::{SIGINT, SIGTERM};
 use std::sync::Arc;
 use std::thread;
 
 pub fn init(shutdown: Arc<Shutdown>) {
-    let signals = Signals::new(&[SIGINT, SIGTERM]).expect("failed to init signal handler");
+    let mut signals = Signals::new(&[SIGINT, SIGTERM]).expect("failed to init signal handler");
     thread::spawn(move || {
         for signal in signals.forever() {
             info!("signal: {}", signal);
