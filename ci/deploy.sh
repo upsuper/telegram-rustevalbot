89c959aafdfd3a423144d9fa00898facbf686dec
Xidorn Quan <me@upsuper.org>
2021-06-19 15:23:43 +1000

    Improve deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
 mkdir -m0700 /tmp/rustevalbot
 git rev-parse HEAD > /tmp/rustevalbot/upgrade
 mkdir -p -m0700 $HOME/.ssh
-cat server_ssh_key >> $HOME/.ssh/known_hosts
+cat ci/server_ssh_key >> $HOME/.ssh/known_hosts
 echo "$DEPLOY_KEY" > /tmp/rustevalbot/deploy_key
 sftp -i /tmp/rustevalbot/deploy_key \
-     -b deploy.sftp \
+     -b ci/deploy.sftp \
      -P 2222 \
      rustevalbot@vps11.upsuper.org
 rm -r /tmp/rustevalbot
